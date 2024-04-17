fab3765a4c418fa737f0dd1c97262894d48930fa
Joey <145919209+Swifty433@users.noreply.github.com>
2024-04-17 12:34:13 +0100

    Fixes made, code works for freature 9, 10

--- src/main/java/org/example/MultiThreaded/Client.java
+++ src/main/java/org/example/MultiThreaded/Client.java
@@ -32,14 +32,14 @@ public class Client {
             Scanner consoleInput = new Scanner(System.in);
             System.out.println("Valid commands include: DisplayID <ID>, DisplayAll, AddEntity , Quit");
             System.out.println("Enter your command:");
-            String userCommand = consoleInput.nextLine();
+            String userCommand = consoleInput.nextLine().toLowerCase();
 
             while (true) {
 
                 out.println(userCommand); //this is required to have(writes the request to socket along with a newline terminator.
 
                 //types of commands go here, Process the answers returned by the server
-                if(userCommand.startsWith("DisplayId")) {
+                if(userCommand.startsWith("displayid")) {
 
                     //sends command to server
                     String jsonMovie = in.readLine(); //receives response from server
@@ -51,8 +51,9 @@ public class Client {
                     //System.out.println(jsonMovie.toString());
                 }
 
-                else if(userCommand.startsWith("DisplayAll")) {
+                else if(userCommand.startsWith("displayall")) {
                     String jsonMovies = in.readLine();
+                    //System.out.println(jsonMovies);
                     List<Movie> movies = JsonConverter.jsonToMovies(jsonMovies);
 
                     for (Movie movie : movies) {
@@ -60,58 +61,58 @@ public class Client {
                     }
                 }
                 else if(userCommand.startsWith("AddEntity")) {
-
-                    //entering data to send back to the server
-
-                    System.out.println("Adding a Movie");
-
-                    try(Scanner keyboard1 = new Scanner(System.in)){
-                        System.out.println("Please enter the movie name");
-                        String movieName = keyboard1.nextLine();
-
-                        System.out.println("Please enter the movie ID");
-                        int movieID = keyboard1.nextInt();
-                        keyboard1.nextLine(); // clear the buffer
-
-                        System.out.println("Please enter the movie's director");
-                        String movieDirector = keyboard1.nextLine();
-
-                        System.out.println("Please enter the movie genre");
-                        String movieGenre = keyboard1.nextLine();
-
-                        System.out.println("Please enter the movie's release year (0000 format)");
-                        int movieYear = keyboard1.nextInt();
-                        keyboard1.nextLine();
-
-                        System.out.println("Please enter the movie rating");
-                        double movieRating = keyboard1.nextDouble();
-                        keyboard1.nextLine();
-
-                        System.out.println("Please enter the movie run-time");
-                        int movieTime = keyboard1.nextInt();
-                        keyboard1.nextLine();
-
-
-
-                        //Serialize the data into a JSON formatted request and send the JSON request to the server
-                        //Basically just format the entered data into json manually and send it back
-
-
-                        String sendJsonData = "{\"name\": \"" + movieName + "\", \"id\": " + movieID + ", \"director\": \"" + movieDirector + "\", \"genre\": \"" + movieGenre + "\", \"release_year\": " + movieYear + ", \"rating\": " + movieRating + ", \"runtime_minutes\": " + movieTime + "}";
-                        //String sendJsonData = "{" + "'id': " + movieID + ", name": "" + movieName + """ + ", "release_year": " + movieYear + ", "genre": "" + movieGenre + """ + ", "director": "" + movieDirector + """ + ", "runtime_minutes": " + movieTime + ", "rating": " + movieRating + "}";
-                        //String sendJsonData = "{" + "id: " + movieID + ",name: " + movieName + "Year" + movieYear + "Genre" + movieGenre + "Director" + movieDirector + "Runtime" + movieTime + "Rating" + movieRating;
-
-                        //writing json data to the output stream
-                        out.println(sendJsonData);
-
-                        //flushing stream to clear buffer
-                        out.flush();
-
-                        //catch incase incorrect data is entered
-                    }   catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
+//
+//                    //entering data to send back to the server
+//
+//                    System.out.println("Adding a Movie");
+//
+//                    try(Scanner keyboard1 = new Scanner(System.in)){
+//                        System.out.println("Please enter the movie name");
+//                        String movieName = keyboard1.nextLine();
+//
+//                        System.out.println("Please enter the movie ID");
+//                        int movieID = keyboard1.nextInt();
+//                        keyboard1.nextLine(); // clear the buffer
+//
+//                        System.out.println("Please enter the movie's director");
+//                        String movieDirector = keyboard1.nextLine();
+//
+//                        System.out.println("Please enter the movie genre");
+//                        String movieGenre = keyboard1.nextLine();
+//
+//                        System.out.println("Please enter the movie's release year (0000 format)");
+//                        int movieYear = keyboard1.nextInt();
+//                        keyboard1.nextLine();
+//
+//                        System.out.println("Please enter the movie rating");
+//                        double movieRating = keyboard1.nextDouble();
+//                        keyboard1.nextLine();
+//
+//                        System.out.println("Please enter the movie run-time");
+//                        int movieTime = keyboard1.nextInt();
+//                        keyboard1.nextLine();
+//
+//
+//
+//                        //Serialize the data into a JSON formatted request and send the JSON request to the server
+//                        //Basically just format the entered data into json manually and send it back
+//
+//
+//                        String sendJsonData = "{\"name\": \"" + movieName + "\", \"id\": " + movieID + ", \"director\": \"" + movieDirector + "\", \"genre\": \"" + movieGenre + "\", \"release_year\": " + movieYear + ", \"rating\": " + movieRating + ", \"runtime_minutes\": " + movieTime + "}";
+//                        //String sendJsonData = "{" + "'id': " + movieID + ", name": "" + movieName + """ + ", "release_year": " + movieYear + ", "genre": "" + movieGenre + """ + ", "director": "" + movieDirector + """ + ", "runtime_minutes": " + movieTime + ", "rating": " + movieRating + "}";
+//                        //String sendJsonData = "{" + "id: " + movieID + ",name: " + movieName + "Year" + movieYear + "Genre" + movieGenre + "Director" + movieDirector + "Runtime" + movieTime + "Rating" + movieRating;
+//
+//                        //writing json data to the output stream
+//                        out.println(sendJsonData);
+//
+//                        //flushing stream to clear buffer
+//                        out.flush();
+//
+//                        //catch incase incorrect data is entered
+//                    }   catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//
 
                 }
                 else if(userCommand.startsWith("Quit")){
@@ -125,10 +126,10 @@ public class Client {
                 }
 
                 //end of while statement clears and prompts the user for a new command
-                consoleInput = new Scanner(System.in);
+                //consoleInput = new Scanner(System.in);
                 System.out.println("Valid commands include: DisplayID<ID>, DisplayAll, AddEntity , Quit");
                 System.out.println("Enter your command:");
-                userCommand = consoleInput.nextLine();
+                userCommand = consoleInput.nextLine().toLowerCase();
             }
         }
 
--- src/main/java/org/example/MultiThreaded/Server.java
+++ src/main/java/org/example/MultiThreaded/Server.java
@@ -93,7 +93,7 @@ class ClientHandler implements Runnable{
 
                 System.out.println("Server: (ClientHandler): Read command from client " + clientNum + ": " + request);
 
-                if (request.startsWith("DisplayId")) {
+                if (request.startsWith("displayid")) {
 
                     //array splits the command in half (DisplayId, <ID>) using the delimiter " "
                     String[] splitCommand = request.split(" ");
@@ -124,7 +124,7 @@ class ClientHandler implements Runnable{
                     }
 
                 }
-                else if (request.startsWith("DisplayAll")) {
+                else if (request.startsWith("displayall")) {
                     try {
                         List<Movie> movies = DAO.getInstance().getAllMovies(); // Retrieve all movies from the DAO
 
@@ -144,31 +144,31 @@ class ClientHandler implements Runnable{
                 }
 
 
-                else if (request.startsWith("AddEntity")) {
+                else if (request.toLowerCase().startsWith("AddEntity")) {
 
-                    // Get the JSON data from the client side
-                    String jsonData = socketReader.readLine();
-
-                    // Convert the data to a Movie object using the new jsonToMovie method added by Emma to the JsonConverter
-                    Movie movie = JsonConverter.jsonToMovie(jsonData, Movie.class);
-
-                    // Add the movie to the database
-                    try {
-                        DAO.getInstance().insertMovie(movie);
-
-                        // Send success confirmation back to the client if everything worked out fine
-                        String successConfirmed = JsonConverter.movietoJson(movie);
-                        socketWriter.println(successConfirmed);
-                        socketWriter.println("Movie added to database.");
-                    } catch (Exception e) {
-                        e.printStackTrace();
-
-                        // Send error response to the client
-                        socketWriter.println("Error: Couldn't add your movie 😦 ");
-                    }
-
-                    // Flush the output stream
-                    socketWriter.flush();
+//                    // Get the JSON data from the client side
+//                    String jsonData = socketReader.readLine();
+//
+//                    // Convert the data to a Movie object using the new jsonToMovie method added by Emma to the JsonConverter
+//                    Movie movie = JsonConverter.jsonToMovie(jsonData, Movie.class);
+//
+//                    // Add the movie to the database
+//                    try {
+//                        DAO.getInstance().insertMovie(movie);
+//
+//                        // Send success confirmation back to the client if everything worked out fine
+//                        String successConfirmed = JsonConverter.movietoJson(movie);
+//                        socketWriter.println(successConfirmed);
+//                        socketWriter.println("Movie added to database.");
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//
+//                        // Send error response to the client
+//                        socketWriter.println("Error: Couldn't add your movie 😦 ");
+//                    }
+//
+//                    // Flush the output stream
+//                    socketWriter.flush();
 
                 }
                 else if (request.startsWith("Quit")) {
